fac04f93fcd77e244deda6556bbebb84c5811c8d
sealucky <sealucky@dmitrys-macbook-pro.local>
2017-11-28 13:48:12 +0300

    Initial Commit

---
+++ Concentration_HomeWork_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration_HomeWork_2
+//
+//  Created by sealucky on 28.11.2017.
+//  Copyright © 2017 sealucky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
